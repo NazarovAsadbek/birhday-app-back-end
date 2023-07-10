2f4c119d333edefeca6f4df587e5fca515d19b21
NazarovAsadbek <nazarov_asadbek_official@mail.ru>
2023-07-10 19:19:19 +0500

    [QA] User module; Create user is done;

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -10,9 +10,9 @@ const ormconfig: ConnectionOptions = {
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: false,
   migrations: [__dirname + '/migrations/**/*{.ts, .js}'],
-  // cli: {
-  //   migrationsDir: 'src/migrations',
-  // },
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
 };
 
 export default ormconfig;
