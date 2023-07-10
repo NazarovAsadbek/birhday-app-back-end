8fb828f021361ca27eb057f18ad5a057679f7001
NazarovAsadbek <nazarov_asadbek_official@mail.ru>
2023-07-10 20:20:56 +0500

    [QA] User module; Added validation pipes;

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UserService } from '@app/user/user.service';
 import { CreateUserDto } from '@app/user/dto/createUser.dto';
 import { UserEntity } from '@app/user/user.entity';
@@ -9,6 +15,7 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('users/create')
+  @UsePipes(new ValidationPipe())
   async createUser(
     @Body('user') createUserDto: CreateUserDto,
   ): Promise<UserResponseInterface> {
